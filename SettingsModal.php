31f7dd20147ffa330cb9aea7976d40688f4a3e24
Thomas Depole <thomas.depole@gmail.com>
2014-02-24 07:56:04 -0500

    First Upload,
    v1.0

---
+++ SettingsModal.php
@@ -0,0 +1,53 @@
+<script>
+$(document).ready(function(){
+	$(".settings").click(function(){
+		$("#SettingsModal").modal();
+	});
+	
+	$("#SettingsModal input[name=daylength]").val(GetSetting("DayLength") / 60);
+	
+	$("#SettingsModal .saveSettings").click(function(){
+		SetSetting("DayLength",  $("#SettingsModal input[name=daylength]").val() * 60);
+		location.reload();
+	});
+});
+	
+</script>
+<!-- The Actual Modal -->
+<div class="modal fade" id="SettingsModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title" id="myModalLabel"></h4>
+			</div>
+			<div class="modal-body">
+				
+				<center>
+					<div class="btn btn-danger " data-dismiss="modal" onclick="ClearDay();">Clear Day</div>
+					<div class="btn btn-info" data-dismiss="modal" onclick="RenderDayReport();"> Show Report</div>
+				</center>
+				
+				<br />
+				
+				<hr />
+				
+				<span class="SectionTitle">Day Progress Bar</span>
+				
+				<!-- Progress bar <input type="radio" name="EnableProgressBar" value="true"> On <input type="radio" name="EnableProgressBar" value="false"> Off
+				
+				<br />
+				-->
+				Day Length: <input type="text" name="daylength" value="" /> In Hours
+				
+			</div>
+			<div class="modal-footer">
+				<button type="buttoon" class="btn btn=default btn-info saveSettings">Save</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
---
+++ TaskModal.php
@@ -0,0 +1,87 @@
+<script>
+	$(".container").delegate(".tasks .task", "click", function(){
+		var id = $(this).attr("data-task_id");
+		$("#TaskModal .starttime").html("Started: " + Tasks[id].StartTime());
+		$("#TaskModal input[name=taskid]").val(id);
+		if(Tasks[id].end > 0){
+			$("#TaskModal .endtime").html("Ended: " + Tasks[id].EndTime());
+			var TimeSpent = GetHoursDifference(Tasks[id].end, Tasks[id].start);
+			$("#TaskModal .timespent").html(TimeSpent);
+		}else{
+			$("#TaskModal .endtime").html("now");
+			var TimeSpent = GetHoursDifference(new Date(), Tasks[id].start);
+			$("#TaskModal .timespent").html(TimeSpent);
+		}
+		
+		// check if task is a resumed task, and change the id to the parent task for the rest of the operations of this modal 
+		var link_id = Tasks[id].link_id;
+		if(link_id != null){
+			id = link_id;
+		}
+		
+		$("#TaskModal input[name=taskid]").val(id);
+		$("#TaskModal input[name=TaskName]").val(Tasks[id].name);
+		$("#TaskModal textarea[name=notes]").val(Tasks[id].notes);
+		$('#TaskModal select[name="tasktype"').html("");
+		for(var i in TaskPickerTypes){
+			var selected = null;
+			if(TaskPickerTypes[i].type == Tasks[id].type) selected = "selected";
+			
+			$('#TaskModal select[name="tasktype"').append('<option value="'+ TaskPickerTypes[i].type +'" '+selected+' >'+ TaskPickerTypes[i].label +'</option>')
+		}
+		
+		$("#TaskModal").modal();
+	});
+	$("body").delegate("#TaskModal .saveTask", "click", function(){
+		var id = $("#TaskModal input[name=taskid]").val();
+		Tasks[id].notes = $("#TaskModal textarea[name=notes]").val();
+		Tasks[id].name = $("#TaskModal input[name=TaskName]").val();
+		Tasks[id].type = $('#TaskModal select[name="tasktype"').val();
+		
+		for(var i in Tasks){
+			if(Tasks[i].link_id == id){
+				Tasks[i].name = $("#TaskModal input[name=TaskName]").val();
+			}	
+		}
+		SaveTasks();
+		RenderTasks(".Tasks");
+		RenderDayProgress();
+		$("#TaskModal").modal('hide');
+	});
+</script>
+
+<!-- The Actual Modal -->
+<div class="modal fade" id="TaskModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<input type="text" name="TaskName" style="border: 1px solid #FFF; width: 400px;" />
+				
+				<h4 class="modal-title" id="myModalLabel"></h4>
+			</div>
+			<div class="modal-body">
+				<div style="float:right"> Task Type <select name="tasktype"></select>  </div>
+				
+				Notes <br />
+				<textarea name="notes"></textarea>
+				
+				<div class="starttime"></div>
+				<div class="endtime"></div>
+				<div class="timespent"></div>
+				
+				
+				<input type="hidden" name="taskid" />		
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary saveTask">Save</button>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
+
